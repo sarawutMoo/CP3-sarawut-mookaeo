8dcfc554b15e2aa65f87e64ecf14dd92256baefc
sarawutMoo <77057574+sarawutMoo@users.noreply.github.com>
2021-01-07 23:05:24 +0700

    Exercise4
    Exercise4

---
+++ assignments/Exercise4_Sarawut_M.py
@@ -0,0 +1,14 @@
+# --- Your Score ---
+# Foundation English : คะแนนที่ได้
+# General Business : คะแนนที่ได้
+# Introduction to Computer Systems : คะแนนที่ได้
+# Computer Programming : คะแนนที่ได้
+FEscore = 78
+GBscore = 80
+ICSscore = 67
+CPscore = 99
+print('--- Your Score ---)')
+print('Foundation English               : ',FEscore)
+print('General Business                 : ',GBscore)
+print('Introduction to Computer Systems : ',ICSscore)
+print('Computer Programming             : ',CPscore)
