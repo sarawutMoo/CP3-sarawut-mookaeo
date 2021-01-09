b1fcdd931379b796eeb5c0f48f82206c77ecf4fa
sarawutMoo <77057574+sarawutMoo@users.noreply.github.com>
2021-01-09 20:04:59 +0700

    exercise5.1
    + - * /

---
+++ assignments/Exercise5_1_Sarawut_M.py
@@ -0,0 +1,12 @@
+# โจทย์
+# 10 + 2 = 12
+# 10 - 2 = 8
+# 10 * 2 = 20
+# 10 / 2 = 5
+num1 = int(input('เลขอะไรก็ได้ตัวแรก : '))
+num2 = int(input('เลขอะไรก็ได้ตัวที่สอง : '))
+#จากโจทย์ ต้องใส่เลข 10 และ 2
+print(num1,'+',num2 ,'=',num1+num2)
+print(num1,'-',num2 ,'=',num1-num2)
+print(num1,'*',num2 ,'=',num1*num2)
+print(num1,'/',num2 ,'=',int(num1/num2))
