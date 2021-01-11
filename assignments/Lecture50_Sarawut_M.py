f570a4b8348c6bf2b7205f7784474c6d5c5fa266
sarawutMoo <77057574+sarawutMoo@users.noreply.github.com>
2021-01-11 21:59:38 +0700

    lecture50
    function

---
+++ assignments/Lecture50_Sarawut_M.py
@@ -0,0 +1,12 @@
+def addNumber (x,y):
+    print(x+y)
+def minusNumber(x,y) :
+    print(x-y)
+def mulNumber (x,y) :
+    print(x*y)
+def divNumber(x,y) :
+    print(x/y)
+addNumber(3,4)
+minusNumber(3,4)
+mulNumber(3,4)
+divNumber(3,4)
