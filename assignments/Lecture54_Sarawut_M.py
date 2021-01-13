3523042c34421a612ee001d660d9b2296a4a2d8d
sarawutMoo <77057574+sarawutMoo@users.noreply.github.com>
2021-01-13 20:23:46 +0700

    lecture54_devFuction
    function vat calculate

---
+++ assignments/Lecture54_Sarawut_M.py
@@ -0,0 +1,40 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+for i in range(3) :
+    if login() and True :
+        print(showMenu())
+        select = menuSelect()
+        if select == 1 :
+            selectOne = int(input("Price is :"))
+            print("Price include is :",vatCalculator(selectOne))
+        elif select == 2 :
+            print("Price include is :",priceCalculator())
+        else:
+             print("Please select 1 or 2 only !!")
+        break
+    else :
+        print("User or Password Incorrect...Please again!!\n")
